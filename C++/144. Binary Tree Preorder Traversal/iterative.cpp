e5ac6816268b629e741bf4539da73e2c7efde602
progresivoJS <progresivoJS@gmail.com>
2017-12-06 13:18:50 +0900

    preorder traversal iteratively using stack

---
+++ C++/144. Binary Tree Preorder Traversal/iterative.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder;
+        if (!root) return preorder;
+        stack<TreeNode*> s;
+        s.push(root);
+        while (!s.empty()) {
+            TreeNode* current = s.top();
+            s.pop();
+            preorder.push_back(current->val);
+            if (current->right)
+                s.push(current->right);
+            if (current->left)
+                s.push(current->left);
+        }
+        return preorder;
+    }
+};
