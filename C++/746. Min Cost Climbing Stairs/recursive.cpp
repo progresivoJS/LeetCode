3ca738013e4cbb113070d5d74501278e757385ec
HwangJinSeong <progresivoJS@gmail.com>
2017-12-26 09:38:41 +0900

    746

---
+++ C++/746. Min Cost Climbing Stairs/recursive.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    vector<int> cache;
+    int climb(vector<int>& cost, int index) {
+        if (index == cost.size()) {
+            return 0;
+        }
+        if (index > cost.size()) {
+            return INT_MAX;
+        }
+        
+        int& ret = cache[index];
+        if (ret != -1) {
+            return ret;
+        }
+        
+        int minimum = cost[index] + min(climb(cost, index + 1), climb(cost, index + 2));
+        return ret = minimum; 
+    }
+    
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        cache = vector<int>(n, -1);
+        int ret = min(climb(cost, 0), climb(cost, 1));
+        return ret;
+    }
+};
