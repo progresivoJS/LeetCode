4f26408b39d63f82ba8da3cd4f98e59e058260f6
HwangJinSeong <progresivoJS@gmail.com>
2017-12-26 23:34:53 +0900

    Create MLE_recursive-knapsack.cpp

---
+++ C++/494. Target Sum/MLE_recursive-knapsack.cpp
@@ -0,0 +1,31 @@
+// Time : O(n * newTarget)
+// Space : O(n * newTarget)
+
+class Solution {
+private:
+    vector<vector<int> > cache;
+    int knapsack(vector<int>& nums, int start, int target) {
+        if (target < 0) return 0;
+        if (start == nums.size()) return target == 0;
+        
+        int& ret = cache[start][target];
+        if (ret != -1) return ret;
+        
+        int res = 0;
+        res += (knapsack(nums, start + 1, target) + knapsack(nums, start + 1, target - nums[start]));
+        return ret = res;
+    }
+    
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int sum = 0;
+        for (int i : nums) sum += i;
+        int newTarget = sum + S;
+        if (newTarget % 2 != 0) return 0;
+        newTarget /= 2;
+        
+        cache = vector<vector<int> >(nums.size(), vector<int>(newTarget + 1, -1));
+        int ret = knapsack(nums, 0, newTarget);
+        return ret;
+    }
+};
