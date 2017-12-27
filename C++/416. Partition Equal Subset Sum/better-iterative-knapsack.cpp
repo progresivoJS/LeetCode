9887932b45b01cf4e00a6b05206e1f8c25d1deca
HwangJinSeong <progresivoJS@gmail.com>
2017-12-27 10:33:28 +0900

    Create better-iterative-knapsack.cpp

---
+++ C++/416. Partition Equal Subset Sum/better-iterative-knapsack.cpp
@@ -0,0 +1,23 @@
+// Time : O(n * target)
+// Space : O(target)
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for (int num : nums) sum += num;
+        if (sum % 2 != 0) return false;
+        int target = sum / 2;
+        
+        vector<bool> dp(target + 1);
+        dp[0] = true;
+        
+        for (int num : nums) {
+            for (int i = target; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+        
+        return dp[target];
+    }
+};
