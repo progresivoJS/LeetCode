1d36c956d0e488a1a9a8fb196a80fb7c5b4ff2f2
HwangJinSeong <progresivoJS@gmail.com>
2017-12-27 11:29:07 +0900

    Create iterative-knapsack.cpp

---
+++ C++/494. Target Sum/iterative-knapsack.cpp
@@ -0,0 +1,29 @@
+// Time : O(n * target)
+// Space : O(n * target)
+
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int sum = 0;
+        for (int num : nums) sum += num;
+        int target = (S + sum);
+        if (target & 1 || (target >> 1) > sum) return 0;
+        target >>= 1;
+        
+        vector<vector<int> > dp(nums.size() + 1, vector<int>(target + 1, 0));
+        dp[0][0] = 1;
+        
+        for (int i = 1; i <= nums.size(); i++) {
+            for (int j = 0; j <= target; j++) {
+                if (j >= nums[i - 1]) {
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j - nums[i - 1]];
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[nums.size()][target];
+    }
+};
