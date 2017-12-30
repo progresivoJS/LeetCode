e1416480d2287c3075b077cb5ee5bfb7fb283063
HwangJinSeong <progresivoJS@gmail.com>
2017-12-31 01:47:30 +0900

    Create binary-search.cpp

---
+++ C++/278. First Bad Version/binary-search.cpp
@@ -0,0 +1,23 @@
+// Time : O(logn * isBadVersion)
+// Space : O(1)
+
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int lo = 1;
+        int hi = n;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (isBadVersion(mid)) {
+                hi = mid;
+            }
+            else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+};
